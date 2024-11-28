760954e94f6285f379db3b99d945759fafb3ba38
mati <mati@mati>
2024-11-28 04:32:52 +0100

    fix consumer offset

--- data_storage/src/data_storage_service.py
+++ data_storage/src/data_storage_service.py
@@ -1,4 +1,4 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, TopicPartition, OffsetAndMetadata
 from elasticsearch import Elasticsearch
 import json
 print("START FILE\n")
@@ -20,17 +20,28 @@ def wait_for_kafka(consumer, retries=1, delay=5):
 consumer = KafkaConsumer(
     'piwo',
     bootstrap_servers='kafka:9092',
-    # auto_offset_reset='earliest',
-    enable_auto_commit=True,
+    auto_offset_reset='earliest',
+    enable_auto_commit=False,
     group_id='data-storage-service-group',
     value_deserializer=lambda x: json.loads(x.decode('utf-8')),
     key_deserializer=lambda x: x.decode('utf-8')
 
 )
 
+def consumer_from_offset(topic, group_id, offset):
+    """return the consumer from a certain offset"""
+    consumer = KafkaConsumer(bootstrap_servers="kafka:9092", group_id=group_id)
+    tp = TopicPartition(topic=topic, partition=0)
+    consumer.assign([tp])
+    consumer.seek(tp, offset)
+
+    return consumer
+
 if wait_for_kafka(consumer):
     print("Connected to Kafka")
     idx = 0
+    consumer = consumer_from_offset('piwo', 'data-storage-service-group', 0)
+
     for message in consumer:
         print("Message key:", message.key)
         print("Message value:", message.value)
