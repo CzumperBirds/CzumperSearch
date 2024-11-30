39f7a294dc5234918973565104fe098a022df373
Jakub Kryczka <kryczka.jakub.pg2@gmail.com>
2024-11-30 19:47:19 +0100

    Searchbar added

--- frontend/CzumperSearch/src/app/views/home/home.component.ts
+++ frontend/CzumperSearch/src/app/views/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from '../../shared/components/logo-component/logo-component.component';
+import { SearchbarComponent } from '../../shared/components/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LogoComponent],
+  imports: [LogoComponent, SearchbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
