8bcd6c1878da5ece2637688f9574bfda62988267
Oliwier Szypczyn <oli.szypczyn@gmail.com>
2024-12-01 14:47:25 +0100

    Refactor daily trivia RSS feed URL to use config variable

--- data_collection/src/rss/daily_trivia.py
+++ data_collection/src/rss/daily_trivia.py
@@ -1,20 +1,19 @@
 """This module contains the logic to get a random article from the Reddit Today I Learned RSS feed."""
 
-import os
 import random
 import feedparser
 from src.exceptions.trivia_exceprion_factory import RSSParseException, EmptyFeedException
-
-URL = os.getenv("REDDIT_DAILY_TRIVIA_RSS_URL")
+from src.config import DAILY_TRIVIA_URL
 
 
 def get_article_response():
     """Get a random article from the RSS feed."""
-    feed = feedparser.parse(URL)
+    feed = feedparser.parse(DAILY_TRIVIA_URL)
     if feed.bozo:
         raise RSSParseException("Failed to parse RSS feed.", details=str(feed.bozo_exception))
     if not feed.entries:
         raise EmptyFeedException(
-            "The RSS feed is empty.", details=f"No entries found in the feed. Check the URL: {URL}"
+            "The RSS feed is empty.",
+            details=f"No entries found in the feed. Check the URL: {DAILY_TRIVIA_URL}",
         )
     return random.choice(feed.entries)
