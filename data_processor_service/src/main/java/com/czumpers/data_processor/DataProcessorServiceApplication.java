f264a4c65224ee13437abd16e7fee8f528d2645e
Oliwier Szypczyn <oli.szypczyn@gmail.com>
2024-12-02 14:36:28 +0100

    Data processor setup, dockerfiles, jokes json deserialization does not work yet but the docker files build the app and we can see incoming kafka messages

---
+++ data_processor_service/src/main/java/com/czumpers/data_processor/DataProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.czumpers.data_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataProcessorServiceApplication.class, args);
+	}
+
+}
