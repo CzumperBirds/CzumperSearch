b198f23d26ae13aadbec8810d168631401a058af
Oliwier Szypczyn <oli.szypczyn@gmail.com>
2024-12-05 19:24:52 +0100

    Update environment variables for joke and trivia services in Docker configuration

--- data_collection/src/config.py
+++ data_collection/src/config.py
@@ -5,8 +5,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-RUNNING = False
-JOKES_API_URL = os.environ["JOKES_API_URL"]
-TIMEOUT = int(os.environ["TIMEOUT"])
 KAFKA_ADDRESS = os.environ["BOOTSTRAP_SERVERS"]
-DAILY_TRIVIA_URL = os.environ["REDDIT_DAILY_TRIVIA_RSS_URL"]
+
+RUNNING = False
+TIMEOUT = int(os.environ["TIMEOUT"])
+
+JOKES_URL = os.environ["JOKES_URL"]
+ONE_PART_JOKES_TOPIC = os.environ["ONE_PART_JOKES_TOPIC"]
+TWO_PART_JOKES_TOPIC = os.environ["TWO_PART_JOKES_TOPIC"]
+
+DAILY_TRIVIA_URL = os.environ["DAILY_TRIVIA_URL"]
+DAILY_TRIVIA_TOPIC = os.environ["DAILY_TRIVIA_TOPIC"]
