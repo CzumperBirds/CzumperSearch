3743f7f47eb63c6b41f936b4938657ae9eb02d4d
ZZZdream <kmultan1234@gmail.com>
2025-01-02 22:21:07 +0100

    Add python scripts for putting data for test and removing data after test

---
+++ tests/utils/put_data.py
@@ -0,0 +1,46 @@
+import os
+from kafka import KafkaProducer
+import json
+from datetime import datetime
+import random
+
+# Function to create the DailyTriviaConsumed structure
+def create_daily_trivia_data():
+    trivia_data = {
+        "id": f"trivia-{random.randint(1, 10000)}",
+        "title": "Fun Trivia Title",
+        "link": "https://example.com/trivia-link",
+        "author": "Trivia Author",
+        "published": datetime.now().isoformat(),  # ISO 8601 format for current datetime
+        "tags": [
+            {"category": "Science"},
+            {"category": "History"},
+            {"category": "Pop Culture"}
+        ]
+    }
+    return trivia_data
+
+# Function to send data to Kafka
+def send_to_kafka():
+    bootstrap_servers = os.getenv('BOOTSTRAP_SERVERS', 'kafka:9092')
+    # Kafka producer configuration
+    producer = KafkaProducer(
+        bootstrap_servers=[bootstrap_servers],  # Use the environment variable
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
+
+    topic = "daily-trivia"
+
+    # Prepare data
+    data = create_daily_trivia_data()
+
+    # Send data to Kafka topic "daily-trivia"
+    producer.send(topic, data)
+    print(f"Sent data to Kafka: {data}")
+
+    # Close the producer connection
+    producer.flush()
+    producer.close()
+
+if __name__ == "__main__":
+    send_to_kafka()
---
+++ tests/utils/remove_data.py
@@ -0,0 +1,28 @@
+import os
+from elasticsearch import Elasticsearch
+from requests.exceptions import RequestException
+
+def delete_data_from_es():
+    # Read Elasticsearch connection details from environment variable
+    es_host = os.getenv('ELASTICSEARCH_HOST', 'localhost')
+    es_port = os.getenv('ELASTICSEARCH_PORT', 9200)
+
+    # Connect to Elasticsearch instance using environment variables
+    es = Elasticsearch([f'http://{es_host}:{es_port}'])
+
+    # Index name to delete the data from
+    index_name = 'processed-resources'
+
+    try:
+        # Check if the index exists before attempting deletion
+        if es.indices.exists(index=index_name):
+            # Deleting the index
+            es.indices.delete(index=index_name)
+            print(f"Index '{index_name}' and all its data have been deleted.")
+        else:
+            print(f"Index '{index_name}' does not exist.")
+    except RequestException as e:
+        print(f"Error connecting to Elasticsearch: {str(e)}")
+
+if __name__ == "__main__":
+    delete_data_from_es()
