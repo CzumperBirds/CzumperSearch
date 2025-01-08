da4cd5f5e653be56dda2fe76896cd761c5c4a8ad
mati <mati@mati>
2025-01-08 23:04:44 +0100

    finish dcs integration with vault

--- data_collection/src/config.py
+++ data_collection/src/config.py
@@ -10,16 +10,34 @@ VAULT_ADDR = os.getenv("VAULT_ADDR")
 VAULT_TOKEN = os.getenv("VAULT_TOKEN")
 SECRET_PATH = os.getenv("SECRET_PATH")
 
-print("VAULT_ADDR -> ", VAULT_ADDR)
-print("VAULT_TOKEN -> ", VAULT_TOKEN)
-print("SECRET_PATH -> ", SECRET_PATH)
-# client = hvac.Client(url=VAULT_ADDR, token=VAULT_TOKEN)
-# secret_data = client.secrets.kv.v1.read_secret(path=SECRET_PATH)["data"]["data"]
-
-# KAFKA_ADDRESS = secret_data["BOOTSTRAP_SERVERS"]
+kafka_key = os.getenv("KAFKA_ADDR_KEY")
 
 
-KAFKA_ADDRESS = os.environ["BOOTSTRAP_SERVERS"]
+
+client = hvac.Client(url=VAULT_ADDR, token=VAULT_TOKEN)
+secret_data = client.secrets.kv.v1.read_secret(path=SECRET_PATH)["data"]["data"]
+
+class SecretHandler:
+    def __init__(self, secret_data):
+        self.secret_data = secret_data
+
+    def get_secret(self, key):
+        return self.secret_data[key]
+    
+    def __repr__(self):
+        return f"SecretHandler({self.secret_data})"
+    
+sh = SecretHandler(secret_data)
+
+
+KAFKA_ADDRESS = sh.get_secret(kafka_key)
+
+# print("VAULT_ADDR -> ", VAULT_ADDR)
+# print("VAULT_TOKEN -> ", VAULT_TOKEN[0:5], "...")
+# print("SECRET_PATH -> ", SECRET_PATH)
+
+# print("kafka_key -> ", kafka_key)
+# print("KAFKA_ADDRESS -> ", KAFKA_ADDRESS)
 
 RUNNING = False
 TIMEOUT = int(os.environ["TIMEOUT"])
