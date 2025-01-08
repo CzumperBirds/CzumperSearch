8642082245d901596694eb9ecf7e4aac2d1d7672
mati <mati@mati>
2025-01-08 22:19:05 +0100

    finish config file for data_storage

--- data_storage/src/config.py
+++ data_storage/src/config.py
@@ -9,7 +9,15 @@ VAULT_ADDR = os.getenv("VAULT_ADDR")
 VAULT_TOKEN = os.getenv("VAULT_TOKEN")
 SECRET_PATH = os.getenv("SECRET_PATH")
 
+elastic_key = os.getenv("ELASTIC_KEY")
+elastic_value = os.getenv("ELASTIC_VALUE")
+
 client = hvac.Client(url=VAULT_ADDR, token=VAULT_TOKEN)
 secret_data = client.secrets.kv.v1.read_secret(path=SECRET_PATH)["data"]["data"]
 print(secret_data)
 
+ELASTIC_USERNAME = secret_data[elastic_key]
+ELASTIC_PASSWORD = secret_data[elastic_value]
+
+print(ELASTIC_USERNAME)
+print(ELASTIC_PASSWORD)
