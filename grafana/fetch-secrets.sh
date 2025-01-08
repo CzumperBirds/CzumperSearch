49998a3e8f9e52178134c0b99e6f06ef94ec5ea8
mati <mati@mati>
2025-01-08 17:14:54 +0100

    github violations fix

--- grafana/fetch-secrets.sh
+++ grafana/fetch-secrets.sh
@@ -2,7 +2,7 @@
 
 # Fetch the password from Vault
 VAULT_ADDR="http://127.0.0.1:8200"  # Replace with your Vault server URL
-VAULT_TOKEN="hvs.NiDaD59gQXojPDhUH0g0cPoF"  # Replace with your actual Vault token
+VAULT_TOKEN="..."  # Replace with your actual Vault token
 SECRET_PATH="secret/data/grafana/config"  # Vault secret path
 
 # Use the Vault CLI or API to fetch the secret
@@ -15,5 +15,7 @@ export GF_SECURITY_ADMIN_PASSWORD=$PASSWORD
 
 echo $GF_SECURITY_ADMIN_PASSWORD
 # Start Docker Compose with the environment variable
-docker-compose -f docker-compose.prod.yml up
+# docker-compose -f docker-compose.prod.yml up
 
+# curl -s --header "X-Vault-Token: ... " \
+#     --request GET http://vault:8200/v1/secret/data/grafana/config 
